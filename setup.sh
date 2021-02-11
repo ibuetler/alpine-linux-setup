9b8b62d795a32080ee2dfbca71a32667b2c11619
Ivan Buetler <ivan.buetler@compass-security.com>
2021-02-11 12:29:56 +0100

    add repo

--- setup.sh
+++ setup.sh
@@ -3,11 +3,9 @@
 echo "http://dl-cdn.alpinelinux.org/alpine/v3.13/community" >> /etc/apk/repositories
 apk update
 apk upgrade
-apk add curl socat nmap net-tools build-base setxkbmap sudo xrandr bash termite
+apk add curl socat nmap net-tools build-base setxkbmap sudo xrandr bash termite zsh
 apk add setup-xorg-base xfce4 xfce4-terminal ligthdm-gtk-greeter dbus-x11 i3wm
 apk add open-vm-tools open-vm-tools-guestinfo open-vm-tools-deploypkg open-vm-tools-gtk
-apk add zsh chsh
-apk add chsh
 apk add i3status libxcb-dev i3lock xf86-video-vmware dmenu
 apk add mesa-gl glib glapi feh
 apk add firefox-esr
@@ -29,4 +27,4 @@ mkdir -p /home/ibuetler/.config/i3
 cp ./ibuetler/wallpaper/compass.jpg /home/ibuetler/wallpaper/compass.jpg
 cp ./ibuetler/.config/i3/config /home/ibuetler/.config/i3/config
 cp ./ibuetler/.profile /home/ibuetler/.profile
-
+chown -R ibuetler:ibuetler /home/ibuetler
