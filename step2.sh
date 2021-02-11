9460c6b93ea54d5437a9c3d0f93577b59deda428
Ivan Buetler <ivan.buetler@compass-security.com>
2021-02-11 12:51:40 +0100

    update

--- step2.sh
+++ step2.sh
@@ -4,7 +4,7 @@ apk add curl socat nmap net-tools build-base setxkbmap sudo xrandr bash termite 
 setup-xorg-base xfce4 xfce4-terminal lightdm dbus-x11
 apk add open-vm-tools open-vm-tools-guestinfo open-vm-tools-deploypkg open-vm-tools-gtk
 apk add lightdm-gtk-greeter i3wm i3status libxcb-dev i3lock xf86-video-vmware dmenu
-apk add mesa-gl glib glapi feh
+apk add mesa-gl glib feh
 apk add firefox-esr
 apk add accountsservice
 
