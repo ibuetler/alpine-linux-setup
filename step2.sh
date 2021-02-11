c8ec29b35799174f4699918f5c230d53727db791
Ivan Buetler <ivan.buetler@compass-security.com>
2021-02-11 17:55:00 +0100

    update

--- step2.sh
+++ step2.sh
@@ -12,10 +12,12 @@ adduser ibuetler
 mkdir -p /home/ibuetler/wallpaper
 mkdir -p /home/ibuetler/.config/i3
 
-# wallpapers
+# user setup ibuetler
 cp ./ibuetler/wallpaper/compass.jpg /home/ibuetler/wallpaper/compass.jpg
 cp ./ibuetler/.config/i3/config /home/ibuetler/.config/i3/config
 cp ./ibuetler/.profile /home/ibuetler/.profile
+mkdir -p /home/ibuetler/.scripts
+cp ./ibuetler/login-script.sh /home/ibuetler/.scripts/
 chown -R ibuetler:ibuetler /home/ibuetler
 cat ./ibuetler/sudoers >> /etc/sudoers
 
