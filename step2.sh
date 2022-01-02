d91492cc15ad9e83f70302e7b5237c8bc2890b5f
Ivan Buetler <ivan.buetler@compass-security.com>
2022-01-02 14:28:20 +0100

    termite not available any more

--- step2.sh
+++ step2.sh
@@ -1,7 +1,6 @@
 #!/bin/ash
 
-apk add curl socat nmap net-tools build-base setxkbmap sudo xrandr bash termite zsh dbus dbus-x11
-setup-xorg-base xfce4 xfce4-terminal lightdm dbus-x11
+apk add curl socat nmap net-tools build-base setxkbmap sudo xrandr bash zsh dbus dbus-x11 sudo setup-xorg-base xfce4 xfce4-terminal lightdm dbus-x11
 apk add open-vm-tools open-vm-tools-guestinfo open-vm-tools-deploypkg open-vm-tools-gtk
 apk add lightdm-gtk-greeter i3wm i3status libxcb-dev i3lock xf86-video-vmware dmenu
 apk add mesa-gl glib feh firefox-esr accountsservice openvpn
